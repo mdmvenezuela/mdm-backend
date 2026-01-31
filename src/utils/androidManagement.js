e45bc2309fcd30fc06c417e2e8e9a02a06b92be3
mdmvenezuela <fanydegonza@gmail.com>
2026-01-31 14:27:35 -0400

    cambio enrollement token 3

--- src/utils/androidManagement.js
+++ src/utils/androidManagement.js
@@ -26,40 +26,7 @@ async function initAndroidManagement() {
   return androidManagement;
 }
 
-async function createEnrollmentToken() {
-  const credentialsJson = Buffer.from(
-    process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64,
-    'base64'
-  ).toString('utf8');
 
-  const credentials = JSON.parse(credentialsJson);
-
-  const auth = new google.auth.GoogleAuth({
-    credentials,
-    scopes: ['https://www.googleapis.com/auth/androidmanagement'],
-  });
-
-  const androidManagement = google.androidmanagement({
-    version: 'v1',
-    auth,
-  });
-
-  const enterpriseName = process.env.ANDROID_ENTERPRISE_NAME;
-
-  const res = await androidManagement.enterprises.enrollmentTokens.create({
-    parent: enterpriseName,
-    requestBody: {
-      policyName: `${enterpriseName}/policies/default`,
-      allowPersonalUsage: false,
-      duration: '86400s', // 24 horas
-    },
-  });
-
-  return {
-    value: res.data.value,
-    qrCode: res.data.qrCode,
-  };
-}
 
 module.exports = {
   initAndroidManagement,
